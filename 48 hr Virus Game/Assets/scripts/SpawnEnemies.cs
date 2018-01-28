dc25034628e4e1fcec813f8aeae1add2d1eb307b
dkarcadecoder <david.kirkpatrick83@gmail.com>
2018-01-27 21:50:29 -0800

    The start of the Game, piecing together nav mesh with cells

--- 48 hr Virus Game/Assets/scripts/SpawnEnemies.cs
+++ 48 hr Virus Game/Assets/scripts/SpawnEnemies.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnEnemies : MonoBehaviour {
-    public GameObject enemy;                // The enemy prefab to be spawned.
+    public GameObject spawner;                // The enemy prefab to be spawned.
+    public GameObject enemy;
+    public GameObject target;
     public float spawnTime = 3f;            // How long between each spawn.
     public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
 
@@ -18,7 +20,10 @@ public class SpawnEnemies : MonoBehaviour {
         // Find a random index between zero and one less than the number of spawn points.
         int spawnPointIndex = Random.Range(0, spawnPoints.Length);
 
+
         // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
-        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        var temp = Instantiate(spawner, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        temp.GetComponent<BloodCellMovement>().SetTarget(target);
+
     }
 }

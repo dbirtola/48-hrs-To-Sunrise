b7b00255a73830db4d70fdfd53bbf0c74277208e
Anthony Chian <anthony@eduroam-169-233-224-155.ucsc.edu>
2018-01-27 20:20:15 -0800

    Addded Health and Damage

---
+++ 48 hr Virus Game/Assets/Health.cs
@@ -0,0 +1,34 @@
+﻿// Written by anthonychian
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+	
+	public int startingHealth; // 100
+	public int currentHealth;
+	public float despawnTime; // 0.2f
+
+	public bool isDead;
+	public bool damaged;
+
+	void Awake() {
+		currentHealth = startingHealth;
+	}
+
+	void Update () {
+		damaged = false;
+	}
+
+	public void inflictDamage(int dmg) {
+		damaged = true;
+		currentHealth -= dmg;
+		if (currentHealth <= 0 && !isDead)
+			Die ();
+	}
+
+	public void Die() {
+		isDead = true;
+		Destroy (gameObject, despawnTime);
+	}
+}

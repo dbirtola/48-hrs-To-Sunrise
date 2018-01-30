0431c6a72214562d7b3e3f5cab548912871b75d8
dkarcadecoder <david.kirkpatrick83@gmail.com>
2018-01-30 00:25:54 -0800

    Here is the Infection Rate and Explosion upgrades Scripts. -David

---
+++ 48 hr Virus Game/Assets/Scripts/Mutations/ExplosionUpgrade.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionUpgrade : Mutation {
+
+    // Use this for initialization
+    // Use this for initialization
+    override public void Start()
+    {
+        base.Start();
+        PlayerTest.player.spawnedUnitEvent.AddListener(Explode);
+    }
+
+    void Explode(GameObject game) {
+        if (game.GetComponent<Virus>()) {
+            game.AddComponent<ExplosiveBuff>();
+        }
+    }
+    // Update is called once per frame
+    override public void Update()
+    {
+        base.Update();
+    }
+}
--- 48 hr Virus Game/Assets/Scripts/Mutations/InfectionUpgrade.cs
+++ 48 hr Virus Game/Assets/Scripts/Mutations/InfectionUpgrade.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class InfectionUpgrade : Mutation {
 
+    public Virus virus;// will be a Virus
+    
+
 	// Use this for initialization
 	override public void Start () {
         base.Start();
+        PlayerTest.player.killedUnitEvent.AddListener(Infect);
 	}
 	
+    void Infect(GameObject game)
+    {
+
+        if (game.GetComponent<RedBloodCell>()) {
+
+            int chance = upgradeLevel * 5;
+            if (Random.Range(0, 100) <= chance)
+            {
+                Instantiate(virus, game.transform.position, Quaternion.identity);
+            }
+        }
+
+    }
+
 	// Update is called once per frame
 	override public void Update () {
         base.Update();

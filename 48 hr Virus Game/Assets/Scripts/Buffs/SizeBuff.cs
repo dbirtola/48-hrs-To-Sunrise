f28d1ce70f156799f73b0ba7c7c5daed3aac28a5
dkarcadecoder <david.kirkpatrick83@gmail.com>
2018-02-03 16:08:21 -0800

    tanker and blood pressure upgrades -david

---
+++ 48 hr Virus Game/Assets/Scripts/Buffs/SizeBuff.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//this script is currently unused
+public class SizeBuff : Buff {
+
+    //public float size;//Start the multiplier at 1, and increment the size per upgrade
+    //public Rigidbody rb = GetComponent<Rigidbody>();
+
+    public void Activate(int newSize)
+    {
+        // amount = health;
+        // GetComponent<Health>().currentHealth += health;
+       // size += newSize;//increment the size of the Virus
+        //increase the scale of the Virus
+       // GetComponent<Virus>().transform.localScale.Set(GetComponent<Virus>().transform.localScale.x*size, GetComponent<Virus>().transform.localScale.y * size, GetComponent<Virus>().transform.localScale.z * size);
+        //increase the mass of the virus??
+        
+    }
+
+    public void Deactivate()
+    {
+       // GetComponent<Damage>().attackDamage -= amount;
+    }
+}
---
+++ 48 hr Virus Game/Assets/Scripts/Mutations/SpawnUpgrade.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnUpgrade : Mutation {
+
+
+    override public void Start()
+    {
+        base.Start();
+       // PlayerTest.player.spawnedUnitEvent.AddListener(Spawn);
+    }
+
+    void Spawn(GameObject game)
+    {
+                PlayerTest.player.GetComponent<SpawnEnemies>().spawnTime = 20 *(1/5*upgradeLevel);
+    }
+    // Update is called once per frame
+    override public void Update()
+    {
+        base.Update();
+    }
+}
